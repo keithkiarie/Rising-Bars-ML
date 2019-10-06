a40a43b67d86aa40b55196fb5fc7a25ab2851253
Keithkiarie <keithkiarie@gmail.com>
2019-10-06 11:57:12 +0300

    Settings menu removed

--- ui.js
+++ ui.js
@@ -16,34 +16,16 @@ go_to_menu = () => {
         }
     }
     document.getElementById("game_display").style.display = "none";
-    document.getElementById("settings").style.display = "none";
+
     document.getElementById("help").style.display = "none";
 
     document.getElementById("menu").style.display = "block";
 }
 
-open_settings = () => {
-    document.getElementById("game_display").style.display = "none";
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("help").style.display = "none";
-
-    document.getElementById("settings").style.display = "block";
-    try {
-        var controls = JSON.parse(localStorage.controls);
-        if (controls.mobile == "longpress") {
-            document.getElementById("longpress").click();
-        } else {
-            document.getElementById("swipecontrol").click();
-        }
-    } catch (error) {
-        
-    }
-}
 
 open_help = () => {
     document.getElementById("game_display").style.display = "none";
     document.getElementById("menu").style.display = "none";
-    document.getElementById("settings").style.display = "none";
 
     document.getElementById("help").style.display = "block";
 }
