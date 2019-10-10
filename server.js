6662cb6d825bff6cb98b8e726f301e33ce27fcdd
Keithkiarie <keithkiarie@gmail.com>
2019-10-10 11:32:00 +0300

    Socket.io

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 let server = require('http').createServer(app);
-let io = require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
 
 let users = [];
 let connections = [];
@@ -9,6 +9,18 @@ let connections = [];
 server.listen(process.env.PORT || 3000);
 console.log('Server started');
 
-app.get('/', function(req, res) {
+
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function (socket) {
+    connections.push(socket);
+    console.log('\n Connected: %s sockets connected', connections.length);
+
+    socket.on('disconnect', function (data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('\n Disconnected: %s sockets connected', connections.length);
+    });
 });
